b4e8c3222ee43ccf478ced30804046ae355d9123
Mustafa Bhadsorawala <39084056+mustafab0@users.noreply.github.com>
2023-08-19 14:32:36 -0400

    trajectory planner

--- code/trajectory/great_circle_path.py
+++ code/trajectory/great_circle_path.py
@@ -1,135 +1,219 @@
-# #plot a great circle path on a map
-
-# import numpy as np
-# import matplotlib.pyplot as plt 
-
-# # define the great circle path
-# def great_circle_path(lat1, lon1, lat2, lon2, npts):
-#     # convert to radians
-#     lat1 = np.radians(lat1)
-#     lon1 = np.radians(lon1)
-#     lat2 = np.radians(lat2)
-#     lon2 = np.radians(lon2)
-    
-#     # calculate the angle between the two points
-#     d = 2*np.arcsin(np.sqrt(np.sin((lat2-lat1)/2)**2 + np.cos(lat1)*np.cos(lat2)*np.sin((lon2-lon1)/2)**2))
-    
-#     # calculate the coordinates of the points along the great circle path
-#     # use the spherical law of cosines
-#     lat = np.zeros(npts)
-#     lon = np.zeros(npts)
-#     for i in range(npts):
-#         f = float(i)/float(npts-1)
-#         a = np.sin((1-f)*d)/np.sin(d)
-#         b = np.sin(f*d)/np.sin(d)
-#         x = a*np.cos(lat1)*np.cos(lon1) + b*np.cos(lat2)*np.cos(lon2)
-#         y = a*np.cos(lat1)*np.sin(lon1) + b*np.cos(lat2)*np.sin(lon2)
-#         z = a*np.sin(lat1) + b*np.sin(lat2)
-#         lat[i] = np.arctan2(z, np.sqrt(x**2 + y**2))
-#         lon[i] = np.arctan2(y, x)
-        
-#     # convert back to degrees
-#     lat = np.degrees(lat)
-#     lon = np.degrees(lon)
-    
-#     return lat, lon
-
-# # define the start and end points
-# lat1 = 90
-# lon1 = 0
-# lat2 = 0
-# lon2 = -110.0
-
-# # calculate the great circle path
-# npts = 100
-# lat, lon = great_circle_path(lat1, lon1, lat2, lon2, npts)
-
-# # plot the great circle path
-# plt.figure()
-# plt.plot(lon, lat, 'r-')
-# plt.xlabel('Longitude (deg)')
-# plt.ylabel('Latitude (deg)')
-# plt.title('Great Circle Path')
-# plt.show()
-
-
-# # plot the great circle path on a map using cartopy
-# import cartopy.crs as ccrs
-# import cartopy.feature as cfeature
-
-# # create a map projection
-# proj = ccrs.LambertConformal(central_longitude=-100.0, central_latitude=40.0)
-
-# # create a figure
-# fig = plt.figure(figsize=(8,8))
-
-# # add a map to the figure and specify the projection
-# ax = fig.add_subplot(1,1,1, projection=proj)
-
-# # add some map features
-# # ax.add_feature(cfeature.LAND)
-# # ax.add_feature(cfeature.OCEAN)
-# # ax.add_feature(cfeature.COASTLINE)
-# # ax.add_feature(cfeature.BORDERS)
-
-# # plot the great circle path
-# ax.plot(lon, lat, 'r-', transform=ccrs.Geodetic())
-
-# # add a title
-# ax.set_title('Great Circle Path')
-
-# # add a lat/lon grid
-# ax.gridlines()
-
-# # show the figure
-# plt.show()
-
 import numpy as np
 import matplotlib.pyplot as plt
-from mpl_toolkits.mplot3d import Axes3D
 
-# Define the unit vector
-unit_vector = np.array([1, 1, 1])
-# Normalize the vector to make it a unit vector
-unit_vector = unit_vector / np.linalg.norm(unit_vector)
 
-# Create a figure and a 3D subplot
-fig = plt.figure()
-ax = fig.add_subplot(111, projection='3d')
+def plot_vector(unit_vectors):
+    # Create a figure and a 3D subplot
+    fig = plt.figure()
+    ax = fig.add_subplot(111, projection='3d')
 
-# Create spherical coordinates
-u = np.linspace(0, 2 * np.pi, 100)
-v = np.linspace(0, np.pi, 100)
-u, v = np.meshgrid(u, v)
+    # Create spherical coordinates
+    u = np.linspace(0, 2 * np.pi, 20)
+    v = np.linspace(0, np.pi, 20)
+    u, v = np.meshgrid(u, v)
 
-# Convert spherical coordinates to Cartesian coordinates
-x = np.cos(u) * np.sin(v)
-y = np.sin(u) * np.sin(v)
-z = np.cos(v)
+    # Convert spherical coordinates to Cartesian coordinates for unit sphere
+    x = np.cos(u) * np.sin(v)
+    y = np.sin(u) * np.sin(v)
+    z = np.cos(v)
 
-# print(unit_vector)
-# Plot the unit vector as a line
-# ax.plot([0, unit_vector[0]], [0, unit_vector[1]], [0, unit_vector[2]], color='r', lw=3)
-ax.quiver(0, 0, 0, unit_vector[0], unit_vector[1], unit_vector[2], color='b', label='Unit Vector')
-ax.plot(0,0,0, marker='+', color='r', label='Origin')
+    # Plot the unit sphere
+    ax.plot_surface(x, y, z, color = 'b', alpha=0.2)
+    
+    #plot meshgrid
+    ax.plot_wireframe(x, y, z, color = 'k', alpha=0.1)
 
-# Plot the unit sphere 
-ax.plot_surface(x, y, z, color='b', alpha=0.2)
+    # Define a list of colors
+    colors = ['r', 'g', 'b', 'c', 'm', 'y', 'k']
+    # Plot unit vectors
+    for i, unit_vector in enumerate(unit_vectors):
+        # Normalize the vector to make it a unit vector
+        unit_vector = unit_vector / np.linalg.norm(unit_vector)
+        
+        # Plot the unit vector as a quiver with a different color
+        color = colors[i % len(colors)]  # Cycle through colors
+        ax.quiver(0, 0, 0, unit_vector[0], unit_vector[1], unit_vector[2],color=color)
 
-# Set axis labels
-ax.set_xlabel('X')
-ax.set_ylabel('Y')
-ax.set_zlabel('Z')
+    # Plot the origin
+    ax.plot([0], [0], [0], marker='+', color='r', label='Origin')
 
-# Set plot title
-ax.set_title('Unit Sphere')
-ax.axis('equal')
+    # Set axis labels
+    ax.set_xlabel('X')
+    ax.set_ylabel('Y')
+    ax.set_zlabel('Z')
 
-# Enable interactive rotation, panning, and zooming
-ax.view_init(elev=20, azim=30)
-ax.mouse_init()
+    # Set plot title
+    ax.set_title('Unit Sphere')
+    ax.axis('equal')
 
-# Show the plot
-plt.show()
+    # Enable interactive rotation, panning, and zooming
+    ax.view_init(elev=20, azim=30)
+    ax.mouse_init()
+
+    # Show the plot
+    plt.show()
+
+    return ax
+
+
+def euler2rotm(euler_angs):
+    roll = np.deg2rad(euler_angs[0])
+    pitch = np.deg2rad(euler_angs[1])
+    yaw = np.deg2rad(euler_angs[2])
+    R_x = np.array([[1, 0, 0],
+                    [0, np.cos(roll), -np.sin(roll)],
+                    [0, np.sin(roll), np.cos(roll)]
+                    ])
+
+    R_y = np.array([[np.cos(pitch), 0, np.sin(pitch)],
+                    [0, 1, 0],
+                    [-np.sin(pitch), 0, np.cos(pitch)]
+                    ])
+
+    R_z = np.array([[np.cos(yaw), -np.sin(yaw), 0],
+                    [np.sin(yaw), np.cos(yaw), 0],
+                    [0, 0, 1]
+                    ])
+
+    R = R_z @ R_y @ R_x
+
+    return R
+
+
+class Quaternion:
+    def __init__(self, x, y, z, w):
+        self.x = x
+        self.y = y
+        self.z = z
+        self.w = w
+
+def to_quaternion(rpy):
+    # rpy = np.deg2rad(rpy)
+    rpy.roll = np.deg2rad(rpy.roll)
+    rpy.pitch = np.deg2rad(rpy.pitch)
+    rpy.yaw = np.deg2rad(rpy.yaw)
+
+    cr = np.cos(rpy.roll * 0.5)
+    sr = np.sin(rpy.roll * 0.5)
+    cp = np.cos(rpy.pitch * 0.5)
+    sp = np.sin(rpy.pitch * 0.5)
+    cy = np.cos(rpy.yaw * 0.5)
+    sy = np.sin(rpy.yaw * 0.5)
+
+    q = Quaternion(
+        sr * cp * cy - cr * sp * sy,
+        cr * sp * cy + sr * cp * sy,
+        cr * cp * sy - sr * sp * cy,
+        cr * cp * cy + sr * sp * sy
+    )
+
+    return q
+
+class EulerAngles:
+    def __init__(self, roll, pitch, yaw):
+        self.roll = roll
+        self.pitch = pitch
+        self.yaw = yaw
+
+def to_euler_angles(q):
+    angles = EulerAngles(0,0,0)
+
+    # roll (x-axis rotation)
+    sinr_cosp = 2 * (q.w * q.x + q.y * q.z)
+    cosr_cosp = 1 - 2 * (q.x * q.x + q.y * q.y)
+    angles.roll = np.arctan2(sinr_cosp, cosr_cosp)
+
+    # pitch (y-axis rotation)
+    sinp = np.sqrt(1 + 2 * (q.w * q.y - q.x * q.z))
+    cosp = np.sqrt(1 - 2 * (q.w * q.y - q.x * q.z))
+    angles.pitch = 2 * np.arctan2(sinp, cosp) - np.pi / 2
+
+    # yaw (z-axis rotation)
+    siny_cosp = 2 * (q.w * q.z + q.x * q.y)
+    cosy_cosp = 1 - 2 * (q.y * q.y + q.z * q.z)
+    angles.yaw = np.arctan2(siny_cosp, cosy_cosp)
+
+    # Convert to degrees
+    angles.roll = np.rad2deg(angles.roll)
+    angles.pitch = np.rad2deg(angles.pitch)
+    angles.yaw = np.rad2deg(angles.yaw)
+
+    return angles
+
+def quaternion_slerp(qstart, qend, t):
+    # Compute the dot product between the two quaternions
+    q0 = np.array([qstart.x, qstart.y, qstart.z, qstart.w])
+    q1 = np.array([qend.x, qend.y, qend.z, qend.w])
+
+    dot = np.dot(q0, q1)
+    
+    DOT_THRESHOLD = 0.9995
+    if dot > DOT_THRESHOLD:
+        # Linearly interpolate and normalize if inputs are too close
+        result = q0 + t * (q1 - q0)
+        result /= np.linalg.norm(result)
+        return result
+    
+    # Clamp the dot product to stay within the domain of acos()
+    dot = np.clip(dot, -1, 1)
+    
+    # Compute theta_0 and theta
+    theta_0 = np.arccos(dot)
+    theta = theta_0 * t
+    
+    # Compute intermediate quaternion v2
+    v2 = q1 - q0 * dot
+    v2 /= np.linalg.norm(v2)
+    
+    # Slerp interpolation
+    result = q0 * np.cos(theta) + v2 * np.sin(theta)
+
+    q_result = Quaternion(result[0], result[1], result[2], result[3])
+    
+    return q_result
+
+z_ax = np.array([0, 0, 1])
+y_ax = np.array([0, 1, 0])
+x_ax = np.array([1, 0, 0]) 
+
+rpy = np.array([62, 45, 50])  #destination RPY angles  ###Change values here###
+plt_vec = z_ax #initial vector for plot
+
+# Define start and end RPY angles (in degrees)
+start_rpy = EulerAngles(0, 0, 0)
+end_rpy = EulerAngles(rpy[0], rpy[1], rpy[2])
+
+cont = 'Y'
+
+while cont == 'Y':
+    start_quat = to_quaternion(start_rpy)
+    end_quat = to_quaternion(end_rpy)
+
+
+    euls_list = [x_ax, y_ax, z_ax]
+    # Perform Slerp interpolation for different values of t
+    num_points = 10
+    for i in range(num_points + 1):
+        t = i / num_points
+        interpolated_quat = quaternion_slerp(start_quat, end_quat, t)
+        interpolated_rpy = to_euler_angles(interpolated_quat)
+        # print(f"t = {t:.2f}, Interpolated RPY angles: {interpolated_rpy}")
+        euls_list.append(euler2rotm(np.array([interpolated_rpy.roll, interpolated_rpy.pitch, interpolated_rpy.yaw]))@plt_vec)
+
+    ax = plot_vector(euls_list)
+    start_rpy = end_rpy
+    plt_vec = euls_list[-1]
+    cont = input("Continue? (Y/N): ")
+    if cont == 'Y':
+        rpy = np.array([float(input("Enter roll: ")), float(input("Enter pitch: ")), float(input("Enter yaw: "))])
+        end_rpy = EulerAngles(rpy[0], rpy[1], rpy[2])
+    
+    #close the plot window
+    # plt.close('all')
+
+
+    
+
+
 
 
--- code/trajectory/trajectory.ino
+++ code/trajectory/trajectory.ino
@@ -1,17 +1,100 @@
+#include <ArduinoEigen.h>
+#include <ArduinoEigenDense.h>
+#include <ArduinoEigenSparse.h>
 
+
+#include <Slerp.h>
+#include <Quaternion.h>
+
+Eigen::Matrix3d euler2rotm(const Eigen::Vector3d& euler_angs) {
+    double roll = euler_angs[0] * PI / 180.0;
+    double pitch = euler_angs[1] * PI / 180.0;
+    double yaw = euler_angs[2] * PI / 180.0;
+
+    Eigen::Matrix3d R_x;
+    R_x << 1, 0, 0,
+           0, cos(roll), -sin(roll),
+           0, sin(roll), cos(roll);
+
+    Eigen::Matrix3d R_y;
+    R_y << cos(pitch), 0, sin(pitch),
+           0, 1, 0,
+           -sin(pitch), 0, cos(pitch);
+
+    Eigen::Matrix3d R_z;
+    R_z << cos(yaw), -sin(yaw), 0,
+           sin(yaw), cos(yaw), 0,
+           0, 0, 1;
+
+    Eigen::Matrix3d R = R_z * R_y * R_x;
+
+    return R;
+}
+
+Eigen::MatrixX3d interpolate_rpy(const Eigen::Vector3d& start_rpy, const Eigen::Vector3d& end_rpy, int num_points) {
+    Eigen::Vector3d start_end[2] = { start_rpy, end_rpy };
+
+    // Create Slerp object and perform interpolation
+    Quaterniond q_start_end[2];
+    for (int i = 0; i < 2; ++i) {
+        AngleAxisd angle_axis(start_end[i][0] * PI / 180.0, Vector3d::UnitX());
+        angle_axis *= AngleAxisd(start_end[i][1] * PI / 180.0, Vector3d::UnitY());
+        angle_axis *= AngleAxisd(start_end[i][2] * PI / 180.0, Vector3d::UnitZ());
+        q_start_end[i] = angle_axis;
+    }
+
+    Quaterniond q_interp;
+    Eigen::MatrixX3d interpolated_rotations(num_points, 3);
+    for (int i = 0; i < num_points; ++i) {
+        double t = static_cast<double>(i) / (num_points - 1);
+        q_interp = q_start_end[0].slerp(t, q_start_end[1]);
+        Eigen::Vector3d eul_interp = q_interp.toRotationMatrix().eulerAngles(0, 1, 2) * 180.0 / PI;
+        interpolated_rotations.row(i) = eul_interp;
+    }
+
+    return interpolated_rotations;
+}
+
+Eigen::Vector3d z_ax(0, 0, 1);
+Eigen::Vector3d y_ax(0, 1, 0);
+Eigen::Vector3d x_ax(1, 0, 0);
+
+Eigen::Vector3d rpy(45, 120, 270);
+
+Eigen::Vector3d start_rpy(0, 0, 0);
+Eigen::Vector3d end_rpy = rpy;
+int num_points = 10;
+
+Eigen::MatrixX3d interp_rot = interpolate_rpy(start_rpy, end_rpy, num_points);
+
+std::vector<Eigen::Vector3d> euls_list;
+euls_list.push_back(x_ax);
+euls_list.push_back(y_ax);
+euls_list.push_back(z_ax);
 
 void setup() {
-  // put your setup code here, to run once:
-  pinMode(LED_BUILTIN, OUTPUT);
+    Serial.begin(115200);
+    while (!Serial) {
+        // Wait for Serial connection
+    }
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-  digitalWrite(LED_BUILTIN, HIGH);
-  delay(500);
-  digitalWrite(LED_BUILTIN, LOW);
-  delay(500);
+    for (int i = 0; i < num_points; ++i) {
+        Eigen::Matrix3d rotation_matrix = euler2rotm(interp_rot.row(i));
+        Eigen::Vector3d rotated_vector = rotation_matrix * z_ax;
+        euls_list.push_back(rotated_vector);
 
+        // Print rotated vector values
+        Serial.print("Rotated Vector (");
+        Serial.print(rotated_vector[0]);
+        Serial.print(", ");
+        Serial.print(rotated_vector[1]);
+        Serial.print(", ");
+        Serial.print(rotated_vector[2]);
+        Serial.println(")");
+    }
 
-
+    // Stop further execution
+    while (1) {}
 }
