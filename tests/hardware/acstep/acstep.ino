ccb5a22e50126a330218fce55634b5b187d393a5
Mustafa Bhadsorawala <39084056+mustafab0@users.noreply.github.com>
2025-05-12 08:27:28 -0500

    two stepper test

--- tests/hardware/acstep/acstep.ino
+++ tests/hardware/acstep/acstep.ino
@@ -1,34 +1,56 @@
-/*
-    Controlling two stepper with the AccelStepper library
-
-     by Dejan, https://howtomechatronics.com
-*/
-
 #include <AccelStepper.h>
+#include <MultiStepper.h>
 
-// Define the stepper motor and the pins that is connected to
-AccelStepper stepper1(1, 15, 16); // (Typeof driver: with 2 pins, STEP, DIR)
-int step_per_rev = 200;
-int microstepping = 2;
-int rpm = 500;
+#define   EN_PIN_R2 11
+#define STEP_PIN_R2 3
+#define  DIR_PIN_R2 2
+
+#define   EN_PIN_P2 12
+#define STEP_PIN_P2 22
+#define  DIR_PIN_P2 23
+
+// Define two steppers: change pins to your wiring
+AccelStepper stepper1(AccelStepper::DRIVER, STEP_PIN_R2, DIR_PIN_R2);
+AccelStepper stepper2(AccelStepper::DRIVER, STEP_PIN_P2, DIR_PIN_P2);
+
+MultiStepper steppers;
 
 void setup() {
-  Serial.begin(9600);
-  stepper1.setMaxSpeed(2500); // Set maximum speed value for the stepper
-  stepper1.setAcceleration(400); // Set acceleration value for the stepper
-  stepper1.setCurrentPosition(0); // Set the current position to 0 steps
 
+  pinMode(EN_PIN_P2, OUTPUT);
+  pinMode(EN_PIN_R2, OUTPUT);
+  digitalWrite(EN_PIN_P2,LOW);
+  digitalWrite(EN_PIN_R2,LOW);
+  // Set max speed and acceleration for each
+  stepper1.setMaxSpeed(1000);
+  stepper1.setAcceleration(500);
+
+  stepper2.setMaxSpeed(1000);
+  stepper2.setAcceleration(500);
+
+  // Tell the MultiStepper object about each motor
+  steppers.addStepper(stepper1);
+  steppers.addStepper(stepper2);
 }
 
 void loop() {
+  long positions[2];
 
-  stepper1.moveTo(step_per_rev*microstepping*rpm); // Set desired move: 800 steps (in quater-step resolution that's one rotation)
-  stepper1.runToPosition(); // Moves the motor to target position w/ acceleration/ deceleration and it blocks until is in position
+  // Define your target positions (in steps) for the two motors:
+  positions[0] = 2000;   // target for stepper1
+  positions[1] = -1500;  // target for stepper2
 
+  // This call will block until both steppers have reached their targets
+  steppers.moveTo(positions);
+  steppers.runSpeedToPosition();
 
-  // Move back to position 0, using run() which is non-blocking - both motors will move at the same time
-  stepper1.moveTo(0);
-  stepper1.runToPosition(); // Moves the motor to target position w/ acceleration/ deceleration and it blocks until is in position
+  delay(1000);
 
-  
+  // You can reverse them and send again, etc.
+  positions[0] = 0;
+  positions[1] = 0;
+  steppers.moveTo(positions);
+  steppers.runSpeedToPosition();
+
+  delay(1000);
 }
