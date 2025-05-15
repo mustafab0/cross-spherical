f1c7629d14f8ac75d90b33c183503fd40b8d5d67
mustafab0 <mustafabhadsorawala@gmail.com>
2025-05-15 00:48:44 -0500

    choreographed one stanza

--- tests/hardware/sia_chandelier/sia_chandelier.ino
+++ tests/hardware/sia_chandelier/sia_chandelier.ino
@@ -1,6 +1,15 @@
 #include <AccelStepper.h>
 #include <MultiStepper.h>
 
+// Motor pin definitions
+#define   EN_PIN_P1 9
+#define STEP_PIN_P1 20
+#define  DIR_PIN_P1 21
+
+#define   EN_PIN_R1 10
+#define STEP_PIN_R1 5
+#define  DIR_PIN_R1 4
+
 #define   EN_PIN_R2 11
 #define STEP_PIN_R2 3
 #define  DIR_PIN_R2 2
@@ -10,54 +19,83 @@
 #define  DIR_PIN_P2 23
 
 // Stepper setup
-AccelStepper stepper1(AccelStepper::DRIVER, STEP_PIN_R2, DIR_PIN_R2); // Roll motor
-AccelStepper stepper2(AccelStepper::DRIVER, STEP_PIN_P2, DIR_PIN_P2); // Pitch motor
+AccelStepper stepper1(AccelStepper::DRIVER, STEP_PIN_R1, DIR_PIN_R1); // Roll motor 1
+AccelStepper stepper2(AccelStepper::DRIVER, STEP_PIN_P1, DIR_PIN_P1); // Pitch motor 1
+AccelStepper stepper3(AccelStepper::DRIVER, STEP_PIN_R2, DIR_PIN_R2); // Roll motor 2
+AccelStepper stepper4(AccelStepper::DRIVER, STEP_PIN_P2, DIR_PIN_P2); // Pitch motor 2
+
 MultiStepper steppers;
 
-const float STEPS_PER_DEGREE = 10.0;
+const float STEPS_PER_DEGREE = 17.78;
 
-// Choreographed roll positions in degrees (from user)
-const int numPositions = 8;
+// Choreographed roll positions in degrees
+const int numPositions = 22;
 const float rollDegrees[numPositions] = {
-  0,     // Start
-  30,    // One
-  -30,   // Two
-  -90,   // Three
-  -60,   // One (2)
-  -120,  // Two (2)
-  -180,  // Three (2)
-  0      // Drink (2)
+  0,      // Start
+  30,     // One
+  -30,    // Two
+  -90,    // Three
+  -60,    // One (2)
+  -120,   // Two (2)
+  -180,   // Three (2)
+  -360,   // Drink
+
+  -330,   // One
+  -390,   // Two
+  -450,   // Three
+  -420,   // One (2)
+  -480,   // Two (2)
+  -540,   // Three (2)
+  -720,   // Drink
+
+  -690,   // One
+  -750,   // Two
+  -810,   // Three
+  -780,   // One (2)
+  -840,   // Two (2)
+  -900,   // Three (2)
+  -1080   // Drink
 };
 
-// Durations (ms) between each word
 const int wordDurations[numPositions - 1] = {
-  333,  // One
-  333,  // Two
-  333,  // Three
-  1000, // slight hold
-  333,  // One (2)
-  333,  // Two (2)
-  333   // Three (2)
-};
+  280,  // One
+  280,  // Two
+  280,  // Three
+  280,  // One (2)
+  280,  // Two (2)
+  280,  // Three (2)
+  500,  // Drink
 
+  280,  // One
+  280,  // Two
+  280,  // Three
+  280,  // One (2)
+  280,  // Two (2)
+  280,  // Three (2)
+  500,  // Drink
+
+  280,  // One
+  280,  // Two
+  280,  // Three
+  280,  // One (2)
+  280,  // Two (2)
+  280,  // Three (2)
+  500 // Final Drink hold
+};
 int currentStep = 0;
-unsigned long lastMoveTime = 0;
 bool isMoving = false;
 
 void setup() {
+  // Enable motors
   pinMode(EN_PIN_R2, OUTPUT);
   digitalWrite(EN_PIN_R2, LOW);
 
   pinMode(EN_PIN_P2, OUTPUT);
   digitalWrite(EN_PIN_P2, LOW);
 
-  stepper1.setMaxSpeed(1000);
-  stepper2.setMaxSpeed(1000);
-  stepper1.setAcceleration(500);
-  stepper2.setAcceleration(500);
-
-  steppers.addStepper(stepper1);
-  steppers.addStepper(stepper2);
+  // Configure stepper parameters
+  stepper3.setAcceleration(10000); // High enough to be instant
+  stepper3.setMaxSpeed(25000);     // Allow high-speed moves
 }
 
 void loop() {
@@ -67,20 +105,26 @@ void loop() {
 
     float deltaDeg = degNext - degNow;
     int duration = wordDurations[currentStep];
-    float speedStepsPerSec = (deltaDeg * STEPS_PER_DEGREE) / (duration / 1000.0);
 
-    stepper1.setSpeed(speedStepsPerSec);
-    stepper1.moveTo(degNext * STEPS_PER_DEGREE);
+    // Calculate speed in steps/sec
+    float speedStepsPerSec = abs(deltaDeg * STEPS_PER_DEGREE) / (duration / 1000.0);
+
+    // Apply direction to speed
+    if (deltaDeg < 0) speedStepsPerSec *= -1;
+
+    stepper3.moveTo(degNext * STEPS_PER_DEGREE);
+    stepper3.setSpeed(speedStepsPerSec);
+
     isMoving = true;
   }
 
   if (isMoving) {
-    stepper1.run();
+    stepper3.runSpeedToPosition();
 
-    if (!stepper1.isRunning()) {
+    if (stepper3.distanceToGo() == 0) {
+      delay(100);
       currentStep++;
       isMoving = false;
-      lastMoveTime = millis();
     }
   }
 }
